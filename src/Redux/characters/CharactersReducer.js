25ec2022b4ee789e6bb227787bbbe06437cf43ec
Augusto <jesus_augga_99@icloud.com>
2022-08-07 14:15:56 -0500

    add remove favorite to reducer

--- src/Redux/characters/CharactersReducer.js
+++ src/Redux/characters/CharactersReducer.js
@@ -1,5 +1,6 @@
 const GET_FAVORITE_CHARACTERS = 'Characters/GET_FAVORITE_CHARACTERS';
 const ADD_FAVORITE_CHARACTER = 'Characters/ADD_FAVORITE_CHARACTER';
+const REMOVE_FAVORITE_CHARACTER = 'Characters/REMOVE_FAVORITE_CHARACTER';
 
 const initialState = [];
 
@@ -17,9 +18,20 @@ const getFavorites = () => async (dispatch) => {
 };
 
 const addFavorite = (character) => async (dispatch) => {
+  const favoriteCharacter = character;
+  favoriteCharacter.isFavorite = true;
   dispatch({
     type: ADD_FAVORITE_CHARACTER,
-    payload: character,
+    payload: favoriteCharacter,
+  });
+};
+
+const removeFavorite = (character) => async (dispatch) => {
+  const favoriteCharacter = character;
+  favoriteCharacter.isFavorite = false;
+  dispatch({
+    type: REMOVE_FAVORITE_CHARACTER,
+    payload: favoriteCharacter,
   });
 };
 
@@ -36,6 +48,13 @@ const charactersReducer = (state = initialState, action) => {
       }
       return [...state, action.payload];
 
+    case REMOVE_FAVORITE_CHARACTER:
+      const index = characterIndex(state, action.payload.id); // eslint-disable-line
+      if (characterIndex(state, action.payload.id) === -1) {
+        return state;
+      }
+      return [...state.slice(0, index), ...state.slice(index + 1)];
+
     default:
       return state;
   }
@@ -44,6 +63,7 @@ const charactersReducer = (state = initialState, action) => {
 export {
   getFavorites,
   addFavorite,
+  removeFavorite,
 };
 
 export default charactersReducer;
